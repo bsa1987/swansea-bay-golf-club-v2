18608f0a9ab0afabfc551aa9449b52832dec5b68
bsa1987 <bruno_santos1987@hotmail.com>
2026-06-22 08:32:29 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -82,10 +82,10 @@ export default function Home() {
 
 <div className="flex items-center justify-center gap-6 lg:gap-12 mb-2">
   <img
-    src="/logo.png"
-    alt="Swansea Bay Golf Club Crest"
-    className="h-20 w-20 lg:h-32 lg:w-32 object-contain drop-shadow-2xl"
-  />
+  src="/logo.png"
+  alt="Swansea Bay Golf Club Crest"
+  className="h-28 w-28 lg:h-44 lg:w-44 object-contain drop-shadow-2xl"
+/>
 
   <h1
     className="font-cinzel font-black uppercase leading-none drop-shadow-2xl"
